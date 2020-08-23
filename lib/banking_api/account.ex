1c7a466525593eecbd0e360f4497e72afa173c56
Itay Peceniski <itay.peck@gmail.com>
2020-08-23 19:29:46 -0300

    account model created

---
+++ lib/banking_api/account.ex
@@ -0,0 +1,27 @@
+defmodule BankingApi.Account do
+  @moduledoc """
+  Essa é a Account Model. Este módulo é responsável por mapear os valores do banco de dados com o Elixir e validá-los.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @doc """
+  Essa é a referência no código Elixir para a tabela `accounts` no banco de dados.
+  """
+  schema "accounts" do
+    field :balance, :decimal, precision: 15, scale: 2, default: 1000
+    belongs_to :user, BankingApi.User
+    timestamps()
+  end
+
+  @doc """
+  Essa função converte a informação para o formato do Elixir e às valida.
+  Os parâmetros da função são a `account struct` que será usada para identificar a tabela no banco de dados, e o `params`, que são os dados que serão inseridos no BD
+  """
+  def changeset(account, attrs \\ %{balance: 1000}) do
+    account
+    |> cast(attrs, [:balance])
+    |> validate_required([:balance])
+  end
+end
