ef4a7f1948e08dcc6664a214b348bfe85520d3d0
Itay Peceniski <itay.peck@gmail.com>
2020-08-23 19:22:32 -0300

    account migration file created

---
+++ priv/repo/migrations/20200823185357_create_account.exs
@@ -0,0 +1,17 @@
+defmodule BankingApi.Repo.Migrations.CreateAccount do
+  use Ecto.Migration
+
+  @doc """
+  A função `change` cria a tabela `accounts` no banco de dados
+  O atributo `precision` define a quantidade máxima de dígitos que a colúna `balance` terá
+  O atributo `scale` define a quantidade de casas decimais
+  """
+  def change do
+    create table :accounts do
+      add :balance, :decimal, precision: 15, scale: 2
+      add :user_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+  end
+end
