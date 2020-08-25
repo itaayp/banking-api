e2e34ca669c2dfabd6b361fa16b0d43a17825359
Itay Peceniski <itay.peck@gmail.com>
2020-08-25 00:02:53 -0300

    /auth/user api response updated

--- lib/banking_api_web/views/user_view.ex
+++ lib/banking_api_web/views/user_view.ex
@@ -1,11 +1,22 @@
 defmodule BankingApiWeb.UserView do
-    @moduledoc """
-    Essa é a User View. Esse módulo é responsável pelo layout que é apresentado ao usuário após chamar funções referentes ao User Controller
-    """
+  @moduledoc """
+  Essa é a User View. Esse módulo é responsável pelo layout que é apresentado ao usuário após chamar funções referentes ao User Controller
+  """
 
-    use BankingApiWeb, :view
+  use BankingApiWeb, :view
 
-    def render("user.json", %{user: user}) do
-        user
-    end
+  def render("account.json", %{user: user, account: account}) do
+    %{
+      "Informações do usuário": %{
+        "Nome completo": "#{user.first_name} #{user.last_name}",
+        "Email": user.email,
+        "senha cadastrada": user.password,
+        "Tipo de acesso ao sistema": user.role
+      },
+      "Informações da conta": %{
+        "saldo em conta": "R$ #{account.balance},00",
+        "número da conta": account.id
+      }
+    }
+  end
 end
