44d2dcdb93ed215d2becfed3aa0a777a955e6f19
Itay Peceniski <itay.peck@gmail.com>
2020-09-05 16:58:52 -0300

    updated changeset_view documentation

--- lib/banking_api_web/views/changeset_view.ex
+++ lib/banking_api_web/views/changeset_view.ex
@@ -7,6 +7,8 @@ defmodule BankingApiWeb.ChangesetView do
 
   @doc """
   Trata as mensagens de erro passadas através do argumento `changeset`.
+
+  O argumento da função é um `changeset`
   """
   def translate_errors(changeset) do
     Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
@@ -14,6 +16,12 @@ defmodule BankingApiWeb.ChangesetView do
 
   @doc """
   Exibe ao usuário final a mensagem de erro.
+
+  Os argumentos da função são:
+    1. A string `"error.json"`
+    2. Um map que contenha o `changeset` a ser manipulado
+
+    O retorno da função um json que contenha a mensagem de erro
   """
   def render("error.json", %{changeset: changeset}) do
     %{errors: translate_errors(changeset)}
